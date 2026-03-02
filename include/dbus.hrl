12be96b537ea8d01bab0ca116737e52c8ef08b32
Lincoln Bryant <lincoln@lincolnbryant.com>
2026-03-01 20:48:58 -0500

    replace error_logger with logger module

--- include/dbus.hrl
+++ include/dbus.hrl
@@ -8,23 +8,23 @@
 -define(dbus_hrl, true).
 
 -ifndef(debug).
--define(debug(Msg), error_logger:info_msg(Msg)).
--define(debug(Msg, Data), error_logger:info_msg(Msg, Data)).
+-define(debug(Msg), logger:info_msg(Msg)).
+-define(debug(Msg, Data), logger:info_msg(Msg, Data)).
 -endif.
 
 -ifndef(info).
--define(info(Msg), error_logger:info_msg(Msg)).
--define(info(Msg, Data), error_logger:info_msg(Msg, Data)).
+-define(info(Msg), logger:info_msg(Msg)).
+-define(info(Msg, Data), logger:info_msg(Msg, Data)).
 -endif.
 
 -ifndef(warn).
--define(warn(Msg),error_logger:warning_msg(Msg)).
--define(warn(Msg, Data), error_logger:warning_msg(Msg, Data)).
+-define(warn(Msg),logger:warning_msg(Msg)).
+-define(warn(Msg, Data), logger:warning_msg(Msg, Data)).
 -endif.
 
 -ifndef(error).
--define(error(Msg), error_logger:error_msg(Msg)).
--define(error(Msg, Data), error_logger:error_msg(Msg, Data)).
+-define(error(Msg), logger:error_msg(Msg)).
+-define(error(Msg, Data), logger:error_msg(Msg, Data)).
 -endif.
 
 -define(DBUS_VERSION_MAJOR, 1).
